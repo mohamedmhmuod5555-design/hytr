ca9b569f72d905237e264970a1722c60550fa475
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-14 14:17:43 -0700

    Update the best thing 11 (2).py

--- the best thing 11 (2).py
+++ the best thing 11 (2).py
@@ -134,7 +134,7 @@ else:
   time1=st.progress(100)
   for sec in range (60,-1,-1):
     time.sleep(1)
-    ty=time1.(int(sec/60)*100)
+    ty=time1(int(sec/60)*100)
     
   
   st.write(num1,sign,num2)
