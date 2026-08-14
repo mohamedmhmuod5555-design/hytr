31813d9a76a842fa065dceea0463feeab6fa9968
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-14 13:25:00 -0700

    Update the best thing 11 (2).py

--- the best thing 11 (2).py
+++ the best thing 11 (2).py
@@ -94,4 +94,83 @@ if menu == operations[0]:
   st.write("you are in level",int(st.session_state.level))
   st.write("الارواح الحاليه ","❤️ "*st.session_state.hearts)
 else:
-  st.write("hhgyu")
+  import random
+import time
+import streamlit as st
+if 'level' not in st.session_state:
+  st.session_state.level=0
+if 'ran' not in st.session_state or st.session_state.ran < 1:
+  st.session_state.ran=int(20*st.session_state.level)
+if 'num' not in st.session_state:
+  st.session_state.num=0
+if 'sc' not in st.session_state:
+  st.session_state.sc=0
+if 'count' not in st.session_state:
+  st.session_state.count=0
+if 'num1' not in st.session_state:
+ st.session_state.num1=random.randint(1,int(st.session_state.ran))
+if 'num2' not in st.session_state:
+ st.session_state.num2=random.randint(1,int(st.session_state.ran))
+if 'sign' not in st.session_state:
+ st.session_state.sign=random.choice(['+','-','*','/'])
+if 'feed' not in st.session_state:
+ st.session_state.feed=0
+
+
+num1 = st.session_state.num1
+num2 = st.session_state.num2
+sign = st.session_state.sign
+if sign=='+':
+ sc=num1+num2
+if sign=='-':
+ sc=num1-num2
+if sign=='*':
+ sc=num1*num2
+if sign=='/':
+ sc=num1//num2 
+st.title("تحدي 60 ثانيه ")
+if st.button("أبدأ التحدي"):
+ nu=st.empty()
+ for n in range(60):
+  nu-=1
+  st.sleep(1)
+  st.write(nu)
+ st.write(num1,sign,num2)
+ number=st.number_input("ادخل النتيجه ",step=1)
+ if st.button("تأكيد الاجابه"):
+   st.session_state.count += 1
+   if number == sc:
+    st.session_state.num += 1
+    st.session_state.feed="correct"
+   else:
+    st.session_state.feed="false" 
+ if st.session_state.feed=="correct":
+   st.success("انك اسطوره يا عبقري الرياضه ")
+   st.balloons()
+   st.session_state.feed=None
+   st.session_state.num1=random.randint(1,int(st.session_state.ran))
+   st.session_state.num2=random.randint(1,int(st.session_state.ran))
+   st.session_state.sign=random.choice(['+','-','*','/'])
+   time.sleep(1)
+   st.rerun()
+ if st.session_state.feed=="false":
+   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
+   st.session_state.feed=None
+   st.session_state.num1=random.randint(1,int(st.session_state.ran))
+   st.session_state.num2=random.randint(1,int(st.session_state.ran))
+   st.session_state.sign=random.choice(['+','-','*','/'])
+   time.sleep(1)
+   st.rerun()
+
+ if st.session_state.num > 0 and st.session_state.num % 10 == 0:
+   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
+   if st.button("الليفل التالي "):
+      st.balloons()
+      st.session_state.level+=1
+      st.session_state.num=0
+      st.session_state.count=0
+      with open("level_number.txt","w") as f:
+       f.write(str(st.session_state.level))
+ st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions" )
+ st.write("you are in level",int(st.session_state.level))
+
