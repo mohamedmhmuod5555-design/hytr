0f32f2d83187bfbea997d06c650ed2ef8ef08f86
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-08-14 13:00:01 -0700

    Update the best thing 11 (2).py

--- the best thing 11 (2).py
+++ the best thing 11 (2).py
@@ -33,7 +33,7 @@ if 'feed' not in st.session_state:
 if 'hearts' not in st.session_state:
  st.session_state.hearts=3
 menu=st.sidebar.radio("الصفحه لرئيسية", ["ألعب و أصل لليفلات مختلفه ","تحدي كم سؤال تستطيع حله في 60 ثانيه"])
-if menu=="play and reach to levels":
+if menu=="ألعب وأصل لليفلات مختلفه":
   num1 = st.session_state.num1
   num2 = st.session_state.num2
   sign = st.session_state.sign
